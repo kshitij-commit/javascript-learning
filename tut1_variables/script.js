74817b8c1ed45d403807cb48931399e0b7cb00bc
Kshitij Verma <kshitijverma577@gmail.com>
2025-10-18 22:26:51 +0530

    variable.js folder change  to script.js

---
+++ tut1_variables/script.js
@@ -0,0 +1,14 @@
+let country="India";
+let continent="Asia";
+let population=1400000000;
+
+console.log(country);
+
+console.log(continent);
+
+console.log(population);
+
+let container="Laptop";
+console.log(container);
+
+let $function=27;
