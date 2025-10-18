d4e19cfacc10b2fdd7c0a9c2aa228a3df5f71b03
Kshitij Verma <kshitijverma577@gmail.com>
2025-10-18 22:56:05 +0530

    add some dataType

--- tut2_dataType/script.js
+++ tut2_dataType/script.js
@@ -1,6 +1,14 @@
+
 let flag=true;
 console.log(flag);
 
 console.log(typeof true);
 
 console.log(typeof flag);
+
+
+let javascriptIsFun=true;
+console.log(typeof javascriptIsFun);
+
+console.log(typeof 23);
+console.log(typeof 'JavaScript is fun');
